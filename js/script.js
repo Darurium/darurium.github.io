657dbcf3659038cc0765f931aafc25e0bed0f54c
Darurium <darurium@gmail.com>
2020-10-18 10:49:22 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+$(document).ready(function(){
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+  const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    navContainer: false,
+    controls: false,
+    nav: false,    
+    responsive: {      
+      320: {
+        nav: true,
+      }
+    }
+  });
+
+  document.querySelector('.next').addEventListener('click', function() {
+      slider.goTo('next')
     });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+  document.querySelector('.prev').addEventListener('click', function() {
+      slider.goTo('prev') 
+    });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+    
+	function toggleSlide(item) {
+		$(item).each(function(i) {
+			$(this).on('click', function(e) {
+			  e.preventDefault();
+			  $('.catalog-card__content').eq(i).toggleClass('catalog-card__content_active');
+			  $('.catalog-card__list').eq(i).toggleClass('catalog-card__list_active');
+			})
+		})
+	}
+
+	toggleSlide('.catalog-card__more');
+	toggleSlide('.catalog-card__back');
+
+});
+
