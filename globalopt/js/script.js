969a978f86fc313334aadd176aeb2823aec823c3
Darurium <darurium@gmail.com>
2021-01-11 08:02:00 +0500

    Add files via upload

---
+++ globalopt/js/script.js
@@ -0,0 +1,18 @@
+const humburger = document.querySelector('.hamburger'),
+      closeBtn = document.querySelector('.menu__close'),
+      menu = document.querySelector('.menu'),
+      links = document.querySelectorAll('.menu__link');
+
+humburger.addEventListener('click', () => {
+    menu.classList.add('menu__active');
+});
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('menu__active');
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('menu__active');
+    });
+});
