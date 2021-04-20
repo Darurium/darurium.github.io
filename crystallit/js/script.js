d543a0a03a8bc8cc0ff8d815b47d75f5fb07831f
Darurium <darurium@gmail.com>
2021-04-20 05:54:00 +0300

    Add files via upload

--- crystallit/js/script.js
+++ crystallit/js/script.js
@@ -3,7 +3,7 @@ $(function() {
         infinite: true,
         slidesToShow: 2,
         slidesToScroll: 1,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 3000,
         responsive: [{
               breakpoint: 992,
@@ -13,7 +13,7 @@ $(function() {
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 740,
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
@@ -61,29 +61,47 @@ $(function() {
 
 
 
-// Modal
+// Modals
 
-const modal = document.querySelector('.modal'),
-      overlay = document.querySelector('.overlay'),
-      closeBtn = document.querySelector('.modal__close'),
-      modalBtn = document.querySelector('.header__btn');
+const modals = (triggerSelector, modalSelector, closeSelector) => {
 
-modalBtn.addEventListener('click', () => {
-    overlay.classList.add('overlay__active');
-    document.body.style.overflow = 'hidden';
-});
+	const trigger = document.querySelectorAll(triggerSelector),
+		  modal = document.querySelector(modalSelector),
+		  close = document.querySelector(closeSelector),
+		  windows = document.querySelectorAll('[data-modal]');
 
-closeBtn.addEventListener('click', () => {
-    overlay.classList.remove('overlay__active');
-    document.body.style.overflow = '';
-});
+	trigger.forEach(item => {
+		item.addEventListener('click', () => {
+			windows.forEach(item => {
+				item.style.display = 'none';
+			});
 
-overlay.addEventListener('click', (e) => {
-  if (e.target === overlay) {
-    overlay.classList.remove('overlay__active');
-    document.body.style.overflow = '';
-  }
-});
+			modal.style.display = 'block';
+			document.body.style.overflow = "hidden";
+
+			
+		});
+
+	});
+
+	close.addEventListener('click', () => {
+		modal.style.display = "none";
+		document.body.style.overflow = "";
+	});
+
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal || e.target.parentNode === modal) {
+			modal.style.display = "none";
+			document.body.style.overflow = "";
+		}
+	});
+
+};
+
+modals('.header__btn', '.popup_calc', '.popup_calc__close');
+modals('.popup_calc__btn', '.popup_size', '.popup_size__close');
+
+
 
 // Hamburger - menu
 
@@ -104,4 +122,87 @@ links.forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('menu__active');
     });
+});
+
+const calc = () => {
+
+	const cards = document.querySelectorAll('.popup_calc__card'),
+		  colorBtn = document.querySelectorAll('.popup_calc__item'),
+		  surFaceBtn = document.querySelectorAll('.popup_calc__surface__button');
+
+	function hideCards () {
+		cards.forEach(item => {
+			item.style.display = 'none';
+			// item.classList.remove('popup_calc__card_active');
+		});		
+	}
+
+	function showCard (color='white', surface='gl', i=0) {
+		cards.forEach(item => {
+			if (item.dataset.color == color && item.dataset.surface == surface) {
+				item.style.display = 'block';
+			}
+		});
+		
+	}
+
+	hideCards();
+	showCard();
+	
+
+	colorBtn.forEach(item => {
+		item.addEventListener('click', () => {
+			colorBtn.forEach(item => {
+				item.classList.remove('popup_calc__item_active');
+			});
+			item.classList.add('popup_calc__item_active');
+			let colorTrigger = document.querySelector('.popup_calc__item_active').getAttribute('data-color'),
+			surfaceTrigger = document.querySelector('.popup_calc__surface__button_active').getAttribute('data-surface');
+			console.log(colorTrigger, surfaceTrigger);
+			hideCards();
+			showCard(colorTrigger, surfaceTrigger);
+		});
+	});
+
+	surFaceBtn.forEach(item => {
+		item.addEventListener('click', () => {
+			surFaceBtn.forEach(item => {
+				item.classList.remove('popup_calc__surface__button_active');
+			});
+			item.classList.add('popup_calc__surface__button_active');
+			let colorTrigger = document.querySelector('.popup_calc__item_active').getAttribute('data-color'),
+			surfaceTrigger = document.querySelector('.popup_calc__surface__button_active').getAttribute('data-surface');
+			console.log(colorTrigger, surfaceTrigger);
+			hideCards();
+			showCard(colorTrigger, surfaceTrigger);
+		});
+	});	
+};
+
+calc();
+
+const cards = document.querySelectorAll('.popup_calc__card');
+
+cards.forEach(item => {
+	
+	item.addEventListener('click', (e) => {
+		let target = e.target;
+		
+		if (target.classList.contains('popup_calc__card') || 
+			target.parentNode.classList.contains('popup_calc__card') ||
+			target.parentNode.parentNode.classList.contains('popup_calc__card')) {
+				cards.forEach(item => {
+					item.classList.remove('popup_calc__card_active');
+				});
+				item.classList.add('popup_calc__card_active');
+			}
+	});
+});
+
+// inputs
+
+document.querySelector('.popup_size__width-range').addEventListener('input', () => {
+	let val = document.querySelector('.popup_size__width-range').value;
+	document.querySelector('.popup_size__width-value').innerHTML = val + ' мм';
+	document.querySelector('.popup_size__width-value').style.left = val - 107 + 'px';
 });
